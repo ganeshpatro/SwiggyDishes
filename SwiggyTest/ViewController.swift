180f00179f7373eac1decdeb0e0094f364c39670
Ganesh Patro <ganesh.patro.tech@gmail.com>
2018-08-03 12:53:07 +0530

    Initial Commit

---
+++ SwiggyTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiggyTest
+//
+//  Created by Ganesh Patro on 8/3/18.
+//  Copyright © 2018 Ganesh Patro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
